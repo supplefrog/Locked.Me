c2adbc5d9dc2d8e6ffe3e5a224eeaa33d01bc6ae
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-09-25 20:34:09 +0530

    modified:   .vscode/settings.json 	modified:   app/build.gradle.kts 	modified:   settings.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 
 application {
     // Define the main class for the application.
-    mainClass.set("lockedme.com.App")
+    mainClass.set("lockedme.Executable")
 }
 
 tasks.named<Test>("test") {
